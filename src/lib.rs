04860f707bb5dd8ecde878e00c18dadfcaaba48c
Alexander Hill <me@alxhill.com>
2025-09-02 18:02:58 -0400

    add test + assertions

--- src/lib.rs
+++ src/lib.rs
@@ -17,3 +17,73 @@ pub mod api {
     pub use nominal_api::tonic::io::nominal::scout::api::proto::WriteRequest;
     pub use nominal_api::tonic::io::nominal::scout::api::proto::WriteRequestNominal;
 }
+
+#[cfg(test)]
+mod tests {
+    use std::collections::HashMap;
+    use std::sync::Mutex;
+    use std::time::{Instant, UNIX_EPOCH};
+    use crate::api::*;
+    use crate::consumer::{ConsumerResult, WriteRequestConsumer};
+    use crate::NominalDatasourceStream;
+    use crate::stream::NominalStreamOpts;
+
+    #[derive(Debug)]
+    struct TestDatasourceStream {
+        requests: Mutex<Vec<WriteRequestNominal>>,
+    }
+
+    impl WriteRequestConsumer for &TestDatasourceStream {
+        fn consume(&self, request: &WriteRequestNominal) -> ConsumerResult<()> {
+            self.requests.lock().unwrap().push(request.clone());
+            Ok(())
+        }
+    }
+
+    #[test]
+    fn test_stream() {
+        let test_consumer = Box::new(TestDatasourceStream {
+            requests: Mutex::new(vec![])
+        });
+        let test_consumer = Box::leak(test_consumer);
+        let stream = NominalDatasourceStream::new_with_consumer(&*test_consumer, NominalStreamOpts {
+            max_points_per_record: 1000,
+            max_request_delay: Default::default(),
+            max_buffered_requests: 2,
+            request_dispatcher_tasks: 4,
+        });
+
+        let start = Instant::now();
+        for batch in 0..5 {
+            let mut points = Vec::new();
+            for i in 0..1000 {
+                let start_time = UNIX_EPOCH.elapsed().unwrap();
+                points.push(DoublePoint {
+                    timestamp: Some(Timestamp {
+                        seconds: start_time.as_secs() as i64,
+                        nanos: start_time.subsec_nanos() as i32 + i,
+                    }),
+                    value: (i % 50) as f64,
+                });
+            }
+
+            stream.enqueue(
+                "channel_1".into(),
+                HashMap::from_iter([("batch_id".to_string(), format!("{batch}"))].into_iter()),
+                points,
+            );
+        }
+
+        drop(stream); // wait for points to flush
+
+        let requests = test_consumer.requests.lock().unwrap();
+
+        assert_eq!(requests.len(), 5);
+        let series = requests.get(0).unwrap().series.get(0).unwrap();
+        if let Some(PointsType::DoublePoints(points)) = series.points.as_ref().unwrap().points_type.as_ref() {
+            assert_eq!(points.points.len(), 1000);
+        } else {
+            panic!("unexpected data type");
+        }
+    }
+}
