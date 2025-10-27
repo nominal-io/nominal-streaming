0f3c3d01125d96b1103b43f7d1cc493319b1a835
Joy He <joy@nominal.io>
2025-10-27 19:23:42 -0400

    get rid of health stuff

--- nominal-streaming/src/listener.rs
+++ nominal-streaming/src/listener.rs
@@ -21,39 +21,39 @@ impl NominalStreamListener for LoggingListener {
         error!("{}: {}", message, error);
     }
 }
-
-#[derive(Debug, Clone)]
-pub struct StreamHealthSnapshot {
-    pub total_failed: u64,
-    pub last_enqueue_time: Instant,
-    pub last_failed_time: Instant,
-}
-
-#[derive(Debug)]
-pub struct HealthReporter {
-    health: Arc<Mutex<StreamHealthSnapshot>>,
-}
-
-impl HealthReporter {
-    pub fn new() -> Self {
-        Self {
-            health: Arc::new(Mutex::new(StreamHealthSnapshot {
-                total_failed: 0,
-                last_enqueue_time: Instant::now(),
-                last_failed_time: Instant::now(),
-            })),
-        }
-    }
-
-    pub fn health_snapshot(&self) -> StreamHealthSnapshot {
-        self.health.lock().clone()
-    }
-}
-
-impl NominalStreamListener for HealthReporter {
-    fn on_error(&self, _message: &str, _error: &dyn Error) {
-        let mut health = self.health.lock();
-        health.total_failed += 1;
-        health.last_failed_time = Instant::now();
-    }
-}
+//
+// #[derive(Debug, Clone)]
+// pub struct StreamHealthSnapshot {
+//     pub total_failed: u64,
+//     pub last_enqueue_time: Instant,
+//     pub last_failed_time: Instant,
+// }
+//
+// #[derive(Debug)]
+// pub struct HealthReporter {
+//     health: Arc<Mutex<StreamHealthSnapshot>>,
+// }
+//
+// impl HealthReporter {
+//     pub fn new() -> Self {
+//         Self {
+//             health: Arc::new(Mutex::new(StreamHealthSnapshot {
+//                 total_failed: 0,
+//                 last_enqueue_time: Instant::now(),
+//                 last_failed_time: Instant::now(),
+//             })),
+//         }
+//     }
+//
+//     pub fn health_snapshot(&self) -> StreamHealthSnapshot {
+//         self.health.lock().clone()
+//     }
+// }
+//
+// impl NominalStreamListener for HealthReporter {
+//     fn on_error(&self, _message: &str, _error: &dyn Error) {
+//         let mut health = self.health.lock();
+//         health.total_failed += 1;
+//         health.last_failed_time = Instant::now();
+//     }
+// }
