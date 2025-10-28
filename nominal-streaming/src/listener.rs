70d0e63ec5e3d6e8d7668058871017adcdedace5
Joy He <joy@nominal.io>
2025-10-27 21:14:30 -0400

    more interface

--- nominal-streaming/src/listener.rs
+++ nominal-streaming/src/listener.rs
@@ -11,6 +11,10 @@ use tracing::error;
 
 pub trait NominalStreamListener: Send + Sync + Debug {
     fn on_error(&self, message: &str, error: &dyn Error);
+
+    fn on_points_failed(&self, num_points: usize) {}
+
+    fn on_points_succeeded(&self, num_points: usize) {}
 }
 
 #[derive(Debug, Default, Clone)]
@@ -21,39 +25,3 @@ impl NominalStreamListener for LoggingListener {
         error!("{}: {}", message, error);
     }
 }
-//
-// #[derive(Debug, Clone)]
-// pub struct StreamHealthSnapshot {
-//     pub total_failed: u64,
-//     pub last_enqueue_time: Instant,
-//     pub last_failed_time: Instant,
-// }
-//
-// #[derive(Debug)]
-// pub struct HealthReporter {
-//     health: Arc<Mutex<StreamHealthSnapshot>>,
-// }
-//
-// impl HealthReporter {
-//     pub fn new() -> Self {
-//         Self {
-//             health: Arc::new(Mutex::new(StreamHealthSnapshot {
-//                 total_failed: 0,
-//                 last_enqueue_time: Instant::now(),
-//                 last_failed_time: Instant::now(),
-//             })),
-//         }
-//     }
-//
-//     pub fn health_snapshot(&self) -> StreamHealthSnapshot {
-//         self.health.lock().clone()
-//     }
-// }
-//
-// impl NominalStreamListener for HealthReporter {
-//     fn on_error(&self, _message: &str, _error: &dyn Error) {
-//         let mut health = self.health.lock();
-//         health.total_failed += 1;
-//         health.last_failed_time = Instant::now();
-//     }
-// }
