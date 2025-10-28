adf66f65001d45204a10b4ef0f45386f3a1d3b5a
Joy He <joy@nominal.io>
2025-10-28 19:19:49 -0400

    hm

--- nominal-streaming/src/listener.rs
+++ nominal-streaming/src/listener.rs
@@ -1,20 +1,15 @@
-use std::collections::HashMap;
 use std::error::Error;
 use std::fmt::Debug;
-use std::path::Path;
-use std::path::PathBuf;
-use std::sync::Arc;
-use std::time::Instant;
+use std::panic::RefUnwindSafe;
 
-use parking_lot::Mutex;
 use tracing::error;
 
-pub trait NominalStreamListener: Send + Sync + Debug {
+pub trait NominalStreamListener: Send + Sync + Debug + RefUnwindSafe {
     fn emit_error(&self, message: &str, error: &dyn Error);
 
-    fn on_points_failed(&self, num_points: usize) {}
+    fn on_points_failed(&self, _num_points: usize) {}
 
-    fn on_points_succeeded(&self, num_points: usize) {}
+    fn on_points_succeeded(&self, _num_points: usize) {}
 }
 
 #[derive(Debug, Default, Clone)]
